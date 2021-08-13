e01243d11a228b2cbd80593b0a1c70b25dc793eb
ekko8847 <867635716@qq.com>
2021-08-13 23:12:04 +0800

    day07

--- src/views/product/Spu/List.vue
+++ src/views/product/Spu/List.vue
@@ -46,6 +46,7 @@
                 size="mini"
                 icon="el-icon-info"
                 title="查看SPU的SKU列表"
+                @click="showSkuList(row)"
               >
               </HintButton>
               <el-popconfirm
@@ -96,6 +97,39 @@
       >
       </SkuForm>
     </el-card>
+
+    <el-dialog
+      :title="`${spu.spuName}的sku列表`"
+      :visible.sync="dialogTableVisible"
+      :before-close="dialogClose"
+    >
+      <el-table :data="skuList" v-loading="loading">
+        <el-table-column
+          property="skuName"
+          label="名称"
+          width=""
+        ></el-table-column>
+        <el-table-column
+          property="price"
+          label="价格"
+          width=""
+        ></el-table-column>
+        <el-table-column
+          property="weight"
+          label="地址"
+          width=""
+        ></el-table-column>
+        <el-table-column property="prop" label="默认图片" width="">
+          <template slot-scope="{ row, $index }">
+            <img
+              :src="row.skuDefaultImg"
+              alt=""
+              style="width:100px;height:80px"
+            />
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-dialog>
   </div>
 </template>
 
@@ -120,8 +154,23 @@ export default {
       total: 0,
       //下面两个数据来控制三个页面的切换
       isShowSpuForm: false,
-      isShowSkuForm: false
+      isShowSkuForm: false,
+
+      dialogTableVisible: false,
+      spu: {},
+      skuList: [],
+      //指令加载
+      loading: false
     };
+  },
+  //解决三级的联动的可操作性
+  watch: {
+    isShowSpuForm(newVal, oldVal) {
+      this.isShowList = !newVal;
+    },
+    isShowSkuForm(newVal, oldVal) {
+      this.isShowList = !newVal;
+    }
   },
   methods: {
     changeCategory({ categoryId, level }) {
@@ -196,6 +245,32 @@ export default {
       } catch (error) {
         this.$message.error("请求失败", error.message);
       }
+    },
+    //点击查看spu的sku的回调
+    async showSkuList(row) {
+      this.dialogTableVisible = true;
+      this.spu = row;
+      //请求之前开启loading加载
+      this.loading = true;
+      try {
+        const result = await this.$API.sku.getListBySpuId(row.id);
+        if (result.code === 200 || result.code === 20000) {
+          this.skuList = result.data;
+        } else {
+          this.$message.error("获取sku列表失败!");
+        }
+      } catch (error) {
+        this.$message.error("请求失败!");
+      }
+      //请求之后关闭loading加载(指令方式)
+      this.loading = false;
+    },
+    //dialog框关闭之前的回调
+    dialogClose(done) {
+      //清空data里的数据
+      this.spu = {};
+      this.skuList = [];
+      done();
     }
   }
 };
